19216f7958a6cee681043d0b53b55090abbc2ca0
Salomao-Faleta <faletas41@gmail.com>
2024-02-22 20:58:10 -0300

    Parte 1
    Funcionamento geral pronto

---
+++ src/data/photoList.ts
@@ -0,0 +1,10 @@
+import { Photo } from "@/types/photo";
+
+export const PhotoList:Photo[] = [
+    {id: 1, url: 'foto1.jpg'},
+    {id: 2, url: 'foto2.jpg'},
+    {id: 3, url: 'foto3.jpg'},
+    {id: 4, url: 'foto4.jpg'},
+    {id: 5, url: 'foto5.jpg'},
+    {id: 6, url: 'foto6.jpg'},
+]
